11b18052e67155f7c138ca281935ddf4ed8f0c4c
Gabriel Marques <gabriel.marq94@gmail.com>
2023-03-24 17:54:46 -0300

    Identificando o animal dada as características

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,64 @@
 import java.io.IOException;
 import java.util.Scanner;
-import java.util.Locale;
 
 public class Main {
- 
-    public static void main(String[] args) throws IOException {
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
-        
-        double salary = sc.nextDouble();
-        double aumento = 0.0;
-        int percentual = 0;
-        
-        if (salary <= 400.0) {
-            percentual = 15;
-        }
-        else if (salary <= 800.0) {
-            percentual = 12;
-        }
-        else if (salary <= 1200.0) {
-            percentual = 10;
-        }
-        else if (salary <= 2000.0) {
-            percentual = 7;
-        } else if (salary > 2000.0) {
-            percentual = 4;
-        }
-        
-        aumento = salary / 100 * percentual;
-        salary += aumento;
-        
-        System.out.printf("Novo salario: %.2f\n", salary);
-        System.out.printf("Reajuste ganho: %.2f\n", aumento);
-        System.out.printf("Em percentual: %d %%\n", percentual);
-        
-        sc.close();
-    }
- 
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+
+		String coluna = sc.next();
+		String tipo = sc.next();
+		String alimento = sc.next();
+		String animal = null; //Exercício não especifica como tratar exceções, decidi portanto retornar null.
+		
+		String[] colunas = {"vertebrado", "invertebrado"};
+		String[] tipos = {"ave", "mamifero", "inseto", "anelideo"};
+		String[] alimentos = {"hematofago", "herbivoro", "onivoro", "carnivoro"};
+
+		if (coluna.equals(colunas[0])) {
+			if (tipo.equals(tipos[0])) {
+				if (alimento.equals(alimentos[3])) {
+					animal = "aguia";
+				}
+				else if (alimento.equals(alimentos[2])) {
+					animal = "pombo";
+				}
+			}
+			else if (tipo.equals(tipos[1])) {
+				if (alimento.equals(alimentos[2])) {
+					animal = "homem";
+				}
+				else if (alimento.equals(alimentos[1])){
+					animal = "vaca";
+				}
+
+			}
+		}
+		else if (coluna.equals(colunas[1])) {
+			if (tipo.equals(tipos[2])) {
+				if (alimento.equals(alimentos[0])) {
+					animal = "pulga";
+				}
+				else if (alimento.equals(alimentos[1])) {
+					animal = "lagarta";
+				}
+
+			}
+			else if (tipo.equals(tipos[3])) {
+				if (alimento.equals(alimentos[0])) {
+					animal = "sanguessuga";
+				}
+				else if (alimento.equals(alimentos[2])) {
+					animal = "minhoca";
+
+				}
+
+			}
+		}
+		
+		System.out.println(animal);
+
+		sc.close();
+	}
+
 }
