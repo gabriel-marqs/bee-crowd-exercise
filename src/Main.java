081c16adc8854e93b0adc338477bf56e781343b7
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-17 20:14:49 -0300

    Exibindo o mês baseado no número digitado. Atualizado para Java 8

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,27 @@
 import java.io.IOException;
-import java.util.Locale;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
+
+import javax.swing.text.DateFormatter;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		int num;
 
-		double salario = sc.nextDouble();
-		double imposto = 0; 
-		
-		if (salario <= 2000) {
-			System.out.println("Isento");
-			return;
-		}
-		
-		else {
-		
-		if (salario <= 3000) {
-			imposto = (salario - 2000) * 0.08;
-		}
-		
-		else if (salario <= 4500) {
-			imposto = (salario - 3000) * 0.18 + 80; 
-		}
-		else {
-			imposto = ((salario - 4500) * 0.28) + 80 + 270;
-		}
-		
-		System.out.printf("R$ %.2f%n", imposto);
-		
-		}
-		
+		do {
+			num = sc.nextInt();
+		} while (num < 1 || num > 12);
+
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMMM", new java.util.Locale("en"));
+		String mes = fmt.format(LocalDate.of(2000, num, 1));
+
+		System.out.println(mes);
+
 		sc.close();
+
 	}
 }
