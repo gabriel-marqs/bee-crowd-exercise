b34361cb0792644f6c0f8f0ea9dd563bd1992ac6
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-19 17:35:02 -0300

    Media dos alunos, exame, aprovado e reprovado

--- src/Main.java
+++ src/Main.java
@@ -9,27 +9,42 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		List<Double> list = new ArrayList<>();
-		int qtd = 0;
-		double media = 0;
 		
-		for (int i = 0; i < 6; i++) {
-			double n = sc.nextDouble();
+		List<Double> notas = new ArrayList<>();
+		double n;
+		
+		for (int i = 0; i < 4; i++) {
+			n = sc.nextDouble();
+			notas.add(n);
+		}
+		
+		double media = ((notas.get(0) * 2) + (notas.get(1) * 3) + (notas.get(2) * 4) + notas.get(3)) / 10;
+		
+		if (media >= 7.0) {
+			System.out.println(media);
+			System.out.println("Aluno aprovado.");
+		}
+		
+		else if (media < 5.0) {
+			System.out.println(media);
+			System.out.println("Aluno reprovado.");
+		}
+		
+		else {
+			System.out.println("Aluno em exame.");
+			System.out.print("Nota do exame: ");
+			double exame = sc.nextDouble();
+			media = (media + exame) / 2;
 			
-			if (n > 0) {
-				list.add(n);
-				qtd++;
+			if (media >= 5.0) {
+				System.out.println("Aluno aprovado.");
 			}
+			else {
+				System.out.println("Aluno reprovado.");
+			}
+			
+			System.out.println("Media final: " + media);
 		}
-		
-		for (int i = 0; i < list.size(); i++) {
-			media += list.get(i);
-		}
-		
-		media = media / list.size();
-		
-		System.out.println(qtd + " valores positivos");
-		System.out.printf("%.1f\n", media);
 		
 		sc.close();
 	}
