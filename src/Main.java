ab30b56ac498cb55d1f98f95fac2175104953909
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-20 19:39:48 -0300

    Identificando pares

--- src/Main.java
+++ src/Main.java
@@ -1,57 +1,22 @@
 import java.io.IOException;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		int par = 0;
 		
-		int[] ordenado = new int[3];
-		int[] vetor = new int[ordenado.length];
-		
-		
-		for (int i = 0; i < ordenado.length; i++) {
+		for (int i = 0; i < 5; i++) {
 			int n = sc.nextInt();
-			ordenado[i] = n;
-		}
-		
-		for (int i = 0; i < ordenado.length; i++) {
-			vetor[i] = ordenado[i];
-		}
-		
-		bubbleSort(ordenado);
-		
-		
-		
-		
-		
-		for (int i = 0; i < ordenado.length; i++) {
-			System.out.println(ordenado[i]);
-		}
-		System.out.println();
-		for (int i = 0; i < vetor.length; i++) {
-			System.out.println(vetor[i]);
-		}
-		
-		
-		sc.close();
-	}
-	
-	public static void bubbleSort(int vetor[]) {
-		int aux;
-		int tam = vetor.length;
-
-		for (int i = 0; i < tam - 1; i++) {
-			for (int j = 0; j < tam - 1 - i; j++) {
-				if (vetor[j] > vetor[j + 1]) {
-					aux = vetor[j];
-					vetor[j] = vetor[j + 1];
-					vetor[j + 1] = aux;
-				}
+			if (n % 2 == 0) {
+				par++;
 			}
 		}
+		
+		System.out.println(par + " valores pares");
+		
+		sc.close();
 	}
 }
 
