909e184a432faa42ba3eeb5c9f0bc1226a9de915
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-20 20:05:17 -0300

    Números ímpares abaixo do valor selecionado

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,30 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		int par = 0;
-		int impar = 0;
-		int positivo = 0;
-		int negativo = 0;
 		
-		for (int i = 0; i < 5; i++) {
-			int n = sc.nextInt();
-			if (n % 2 == 0) {
-				par++;
-			}
-			if (n % 2 != 0) {
-				impar++;
-			}
-			if (n < 0) {
-				negativo++;
-			}
-			if (n > 0) {
-				positivo++;
+		List<Integer> impares = new ArrayList<>();
+		int n = sc.nextInt();
+
+		while (n < 1 || n > 1000) {
+			n = sc.nextInt();
+		
+
+		for (int i = n; i > 0; i--) {
+			if (i % 2 != 0) {
+				impares.add(i);
 			}
 		}
-		
-		System.out.println(par + " valor(es) par(es)");
-		System.out.println(impar + " valor(es) impar(es)");
-		System.out.println(positivo + " valor(es) positivo(s)");
-		System.out.println(negativo + " valor(es) negativo(s)");
-		
-		
-		
+
+		for (int i = impares.size() - 1; i >= 0; i--) {
+			int aux = impares.get(i);
+			System.out.println(aux);
+		}
 		sc.close();
 	}
 }
-
