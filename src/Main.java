db17921a7f8758310770146aa323e0d8ccb9e02d
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-22 14:52:32 -0300

    Números que sobram 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -14,32 +12,9 @@ public class Main {
 			n = sc.nextInt();
 		}
 		
-		int[] list = new int[n];
-		
-		for (int i = 0; i < n; i++) {
-			list[i] = sc.nextInt();		
-		}
-		
-		String pI, pN;
-		for (int i = 0; i < n; i++) {
-			if (list[i] == 0) {
-				System.out.println("null");
-			}
-			else {
-				if (list[i] % 2 == 0) {
-					pI = "EVEN";
-				}
-				else {
-					pI = "ODD";
-				}
-				if (list[i] > 0) {
-					pN = "POSITIVE";
-				}
-				else {
-					pN = "NEGATIVE";
-				}
-				
-				System.out.println(pI + " " + pN);
+		for (int i = 0; i <= 10000; i++) {
+			if (i % n == 2) {
+				System.out.println(i);
 			}
 		}
 		
