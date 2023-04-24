a1c1444a540c7134b2c30bd095a7811bc590faeb
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-24 16:15:44 -0300

    média ponderada

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,29 @@
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
 		
-		while (n < 2 || n >= 1000) {
-			n = sc.nextInt();
+		double[] result = new double[n];
+		
+		for (int i = 0; i < n; i++) {
+			double a1 = sc.nextDouble();
+			double a2 = sc.nextDouble();
+			double a3 = sc.nextDouble();
+			
+			result[i] = (a1 * 2 + a2 * 3 + a3 * 5) / 10;			
 		}
 		
-		for (int i = 1; i <= 10; i++) {
-			int mult = n * i;
-			System.out.printf("%d x %d = %d\n", i, n, mult);
+		for (int i = 0; i < n; i++) {
+			System.out.printf("%.1f%n", result[i]);
 		}
+		
 		
 		sc.close();
 	}
