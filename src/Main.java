0603ba6e7abe2f19413cb084c01f0c87042b1fef
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-25 19:52:49 -0300

    Maior valor

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,28 @@
 import java.io.IOException;
-import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		int[] num = new int[3];
+		int pos = 1;
 		
-		int n = sc.nextInt();
-		
-		double[] result = new double[n];
-		
-		for (int i = 0; i < n; i++) {
-			double a1 = sc.nextDouble();
-			double a2 = sc.nextDouble();
-			double a3 = sc.nextDouble();
+		num[0] = sc.nextInt();
+		int maior = num[0];
+		for (int i = 1; i < num.length; i++) {
+			num[i] = sc.nextInt();
+			if (num[i] > maior) {
+				maior = num[i];
+				pos = i + 1;
+			}
 			
-			result[i] = (a1 * 2 + a2 * 3 + a3 * 5) / 10;			
 		}
 		
-		for (int i = 0; i < n; i++) {
-			System.out.printf("%.1f%n", result[i]);
-		}
-		
-		
+		System.out.println(maior);
+		System.out.println(pos);
+
 		sc.close();
 	}
 }
