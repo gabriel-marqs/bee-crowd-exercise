d7b0d4f48a273082d5c979a4bf038d9a627c2493
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-25 20:37:05 -0300

    Controlando experimentos

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,53 @@
 import java.io.IOException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
+		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int[] num = new int[3];
-		int pos = 1;
-		
-		num[0] = sc.nextInt();
-		int maior = num[0];
-		for (int i = 1; i < num.length; i++) {
-			num[i] = sc.nextInt();
-			if (num[i] > maior) {
-				maior = num[i];
-				pos = i + 1;
+		int n = sc.nextInt();
+		sc.nextLine();
+
+		int quantity = 0;
+		int total = 0;
+		int ratos = 0;
+		int sapos = 0;
+		int coelhos = 0;
+		char type = 'w';
+		for (int i = 0; i < n; i++) {
+			String line = sc.nextLine();
+			String[] info = line.split(" ");
+			quantity = Integer.parseInt(info[0]);
+			total += quantity;
+			type = info[1].charAt(0);
+
+			if (type == 'C') {
+				coelhos += quantity;
 			}
-			
+			if (type == 'S') {
+				sapos += quantity;
+			}
+			if (type == 'R') {
+				ratos += quantity;
+			}
+
 		}
 		
-		System.out.println(maior);
-		System.out.println(pos);
+		double coelhosP = ((double)coelhos / total) * 100;
+		double saposP = ((double)sapos / total) * 100;
+		double ratosP = ((double)ratos / total) * 100;
+		
+		System.out.println("Total: " + total + " cobaias");
+		System.out.println("Total de coelhos: " + coelhos);
+		System.out.println("Total de ratos: " + ratos);
+		System.out.println("Total de sapos: " + sapos);
+		System.out.printf("Percentual de coelhos: %.2f %%\n", coelhosP);
+		System.out.printf("Percentual de ratos: %.2f %%\n", ratosP);
+		System.out.printf("Percentual de sapos: %.2f %%\n", saposP);
+		
 
 		sc.close();
 	}
