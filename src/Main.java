96ca70189c19b23915aae6199f00846acdb38cb8
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-26 17:42:38 -0300

    atualizando dois números até um chegar a 0

--- src/Main.java
+++ src/Main.java
@@ -1,54 +1,14 @@
 import java.io.IOException;
-import java.util.Locale;
-import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
-		int n = sc.nextInt();
-		sc.nextLine();
-
-		int quantity = 0;
-		int total = 0;
-		int ratos = 0;
-		int sapos = 0;
-		int coelhos = 0;
-		char type = 'w';
-		for (int i = 0; i < n; i++) {
-			String line = sc.nextLine();
-			String[] info = line.split(" ");
-			quantity = Integer.parseInt(info[0]);
-			total += quantity;
-			type = info[1].charAt(0);
-
-			if (type == 'C') {
-				coelhos += quantity;
-			}
-			if (type == 'S') {
-				sapos += quantity;
-			}
-			if (type == 'R') {
-				ratos += quantity;
-			}
-
+		
+		int j = 60;
+		for (int i = 1; j >= 0; i = i + 3) {
+			System.out.println("I=" + i + " J=" + j);
+			j = j - 5;
 		}
-		
-		double coelhosP = ((double)coelhos / total) * 100;
-		double saposP = ((double)sapos / total) * 100;
-		double ratosP = ((double)ratos / total) * 100;
-		
-		System.out.println("Total: " + total + " cobaias");
-		System.out.println("Total de coelhos: " + coelhos);
-		System.out.println("Total de ratos: " + ratos);
-		System.out.println("Total de sapos: " + sapos);
-		System.out.printf("Percentual de coelhos: %.2f %%\n", coelhosP);
-		System.out.printf("Percentual de ratos: %.2f %%\n", ratosP);
-		System.out.printf("Percentual de sapos: %.2f %%\n", saposP);
-		
-
-		sc.close();
 	}
 }
