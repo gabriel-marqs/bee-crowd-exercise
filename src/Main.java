df06ad8525c226a306a7cffd146f4ba895717822
Gabriel Marques <gabriel.marq94@gmail.com>
2023-04-26 17:52:17 -0300

    Exercitando for ainda

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,11 @@ public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
-		for (int i = 1; i <= 9; i = i + 3) {
+		for (int i = 1; i <= 9; i = i + 2) {
 			for (int j = 7; j > 4; j--) {
 				System.out.println("I="+i + " J="+j);
 			}
-			if (i == 1) {
-				i--;
-			}
-		
+					
 		}
 	}
 }
